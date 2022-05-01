8d6b4048a0502f266324426c7f88fcd20af5a29b
jowongg <heiheiwong77@gmail.com>
2022-05-01 09:31:03 -0400

    main page

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+var ball = document.querySelector(".ball");
+const c1 = document.querySelector(".c1 h5");
 
+
+c1.innerHTML = c1.textContent.replace(/\S/g,
+"<span>$&</span>");
+
+const element =document.querySelectorAll('span');
+for(let i=0;i<element.length;i++)
+{
+    element[i].style.transform ="rotate("+i*19+"deg)";
+}
+
+document.onmousemove = function(){
+    var x = event.clientX *100/ window.innerWidth + "%";
+    var y = event.clientY * 100/ window.innerHeight + "%";
+    console.log(x);
+    ball.style.left =x;
+    ball.style.top=y;
+    ball.style.transform = "translate(-"+x+",-"+y+")";
+}
